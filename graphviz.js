3232e612d7a6214e7807104500686fdbdb5edc61
aswinkarthik93 <aswinkarthik93@gmail.com>
2017-10-06 18:17:58 +0530

    Removing unnecessary logs

--- graphviz.js
+++ graphviz.js
@@ -2,12 +2,9 @@ const shell = require("shelljs")
 
 module.exports = {
   convert: (dotFile, format, outputFile) => {
-    console.log(`Dotfile:${dotFile} Format:${format} outputFile:${outputFile}`)
     if (shell.exec(`dot -T${format} ${dotFile} -o ${outputFile}.${format}`).code !== 0) {
-      console.log("Conversion of dot file failed.")
+      console.log("Execution failed.")
       shell.exit(1)
-    } else {
-      console.log("Conversion successful")
     }
   }
 }
